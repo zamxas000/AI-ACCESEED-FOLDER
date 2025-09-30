797f7d54f275ee461c5d8cd1c8d1b12444876a62
zamxas000 <zamxas000@gmail.com>
2025-09-30 15:08:35 -0400

    test

--- sketch.js
+++ sketch.js
@@ -1,10 +1,36 @@
+let x = 50;
+let speed = 5;
+let colorValue = 128;
+let speedSlider, colorSlider;
+
 function setup() {
     let canvas = createCanvas(400, 400);
     canvas.parent('sketch-container');
     background(220);
+
+    // Get sliders from DOM
+    speedSlider = select('#speedSlider');
+    colorSlider = select('#colorSlider');
 }
 
 function draw() {
-    fill(100, 150, 255);
-    ellipse(width/2, height/2, 100, 100);
+    background(220);
+
+    // Read slider values
+    speed = int(speedSlider.value());
+    colorValue = int(colorSlider.value());
+
+    // Update display values
+    select('#speedValue').html(speed);
+    select('#colorValue').html(colorValue);
+
+    // Move circle
+    x += speed;
+    if (x > width + 50) x = -50;
+
+    // Color interpolation: red to green
+    let r = 255 - colorValue;
+    let g = colorValue;
+    fill(r, g, 0);
+    ellipse(x, height/2, 100, 100);
 }
